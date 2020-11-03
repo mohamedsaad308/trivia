a880e34a0661cfb022491c8629bba1a45c5da2fb
mohamedsaad308 <mohamedsaad308@gmail.com>
2020-11-03 16:29:59 +0200

    Fix PEP8

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -55,7 +55,6 @@ def create_app(test_config=None):
   including pagination (every 10 questions).
   This endpoint should return a list of questions,
   number of total questions, current category, categories.
-
   TEST: At this point, when you start the application
   you should see questions and categories generated,
   ten questions per page and pagination
@@ -91,7 +90,6 @@ def create_app(test_config=None):
     '''
   @TODO:
   Create an endpoint to DELETE question using a question ID.
-
   TEST: When you click the trash icon next to a question,
   the question will be removed.
   This removal will persist in the database and when you refresh the page.
@@ -121,7 +119,6 @@ def create_app(test_config=None):
   Create an endpoint to POST a new question,
   which will require the question and answer text,
   category, and difficulty score.
-
   TEST: When you submit a question on the "Add" tab,
   the form will clear and the question will appear at the end of the last page
   of the questions list in the "List" tab.
@@ -175,7 +172,6 @@ def create_app(test_config=None):
   Create a POST endpoint to get questions based on a search term.
   It should return any questions for whom the search term
   is a substring of the question.
-
   TEST: Search by any phrase. The questions list will update to include
   only question that include that string within their question.
   Try using the word "title" to start.
@@ -185,7 +181,6 @@ def create_app(test_config=None):
     '''
   @TODO:
   Create a GET endpoint to get questions based on category.
-
   TEST: In the "List" tab / main screen, clicking on one of the
   categories in the left column will cause only questions of that
   category to be shown.
@@ -215,7 +210,6 @@ def create_app(test_config=None):
   This endpoint should take category and previous question parameters
   and return a random questions within the given category,
   if provided, and that is not one of the previous questions.
-
   TEST: In the "Play" tab, after a user selects "All" or a category,
   one question at a time is displayed, the user is allowed to answer
   and shown whether they were correct or not.
